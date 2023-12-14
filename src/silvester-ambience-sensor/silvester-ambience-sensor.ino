7967c0c08bc5f18e016ce6146830a74a6b782ae2
JensOstertag <JensOstertag@gmx.de>
2023-12-14 20:14:55 +0100

    Print light sensor readings to serial plotter

--- src/silvester-ambience-sensor/silvester-ambience-sensor.ino
+++ src/silvester-ambience-sensor/silvester-ambience-sensor.ino
@@ -1,14 +1,45 @@
 // WeMos board: https://github.com/esp8266/Arduino
-// LoRa: https://github.com/mcci-catena/arduino-lmic
 
-// CO2 sensor (MQ-135)
-// Sound sensor
-// Light sensor (BH1750)
+// Config
+#include "config.h"
 
-// LoRa (SX1276)
+// LoRa (SX1276) - https://github.com/mcci-catena/arduino-lmic
+// TODO
+
+// Light sensor (BH1750) - https://github.com/claws/BH1750
+#include <BH1750.h>
+#include <Wire.h>
+BH1750 lightSensor(LIGHTSENSOR_I2C_ADDR);
+
+// Sound sensor (Generic microphone module)
+// TODO
 
 void setup() {
+  Serial.begin(BAUD_RATE);
+
+  // LoRa setup
+  // TODO
+
+  // Light sensor setup
+  Wire.begin();
+  if(!lightSensor.begin(LIGHTSENSOR_MODE)) {
+    Serial.println("(Setup) Failed to setup light sensor");
+  }
+
+  // Sound sensor setup
+  // TODO
 }
 
 void loop() {
+  // Light sensor measurement
+  float light = lightSensor.readLightLevel();
+  Serial.print("light:");
+  Serial.print(light);
+
+  // Sound sensor measurement
+  // TODO
+
+  Serial.println();
+
+  delay(100);
 }
