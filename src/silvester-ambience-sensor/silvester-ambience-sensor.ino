0a2c676ef2ecf83ec16b48286b5c106277d3d6b8
JensOstertag <JensOstertag@gmx.de>
2023-12-14 09:23:33 +0100

    Initial commit

---
+++ src/silvester-ambience-sensor/silvester-ambience-sensor.ino
@@ -0,0 +1,14 @@
+// WeMos board: https://github.com/esp8266/Arduino
+// LoRa: https://github.com/mcci-catena/arduino-lmic
+
+// CO2 sensor (MQ-135)
+// Sound sensor
+// Light sensor (BH1750)
+
+// LoRa (SX1276)
+
+void setup() {
+}
+
+void loop() {
+}
