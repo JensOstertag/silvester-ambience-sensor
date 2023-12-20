ea1e307ac4548e2fc992e6c5cc64d6a9320400a1
JensOstertag <JensOstertag@gmx.de>
2023-12-20 08:10:24 +0100

    Implement LoRa setup

--- src/silvester-ambience-sensor/silvester-ambience-sensor.ino
+++ src/silvester-ambience-sensor/silvester-ambience-sensor.ino
@@ -4,7 +4,27 @@
 #include "config.h"
 
 // LoRa (SX1276) - https://github.com/mcci-catena/arduino-lmic
-// TODO
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+const lmic_pinmap lmic_pins = {
+  .nss = LORA_PIN_CS,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = LMIC_UNUSED_PIN,
+  .dio = {LORA_PIN_DIO0, LORA_PIN_DIO1, LMIC_UNUSED_PIN}
+};
+void os_getArtEui(u1_t* buf) {
+  u1_t appEui[8] = TTN_APP_EUI;
+  memcpy_P(buf, appEui, 8);
+}
+void os_getDevEui(u1_t* buf) {
+  u1_t devEui[8] = TTN_DEV_EUI;
+  memcpy_P(buf, devEui, 8);
+}
+void os_getDevKey(u1_t* buf) {
+  u1_t appKey[16] = TTN_APP_KEY;
+  memcpy_P(buf, appKey, 16);
+}
 
 // Light sensor (BH1750) - https://github.com/claws/BH1750
 #include <BH1750.h>
@@ -18,7 +38,9 @@ void setup() {
   Serial.begin(BAUD_RATE);
 
   // LoRa setup
-  // TODO
+  os_init();
+  LMIC_reset();
+  LMIC_setClockError(MAX_CLOCK_ERROR * LORA_CLOCK_ERROR);
 
   // Light sensor setup
   Wire.begin();
