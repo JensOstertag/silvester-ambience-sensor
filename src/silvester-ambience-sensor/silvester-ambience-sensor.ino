74921ff2c3e381bbef6eb14647e94975e23deb53
JensOstertag <JensOstertag@gmx.de>
2023-12-22 07:58:36 +0100

    Prepare LoRaWAN implementation

--- src/silvester-ambience-sensor/silvester-ambience-sensor.ino
+++ src/silvester-ambience-sensor/silvester-ambience-sensor.ino
@@ -25,6 +25,85 @@ void os_getDevKey(u1_t* buf) {
   u1_t appKey[16] = TTN_APP_KEY;
   memcpy_P(buf, appKey, 16);
 }
+void onEvent(ev_t event) {
+  switch(event) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println("EV_SCAN_TIMEOUT");
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println("EV_BEACON_FOUND");
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println("EV_BEACON_MISSED");
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println("EV_BEACON_TRACKED");
+      break;
+    case EV_JOINING:
+      Serial.println("EV_JOINING");
+      break;
+    case EV_JOINED:
+      Serial.println("EV_JOINED");
+      LMIC_setLinkCheckMode(0);
+      break;
+    case EV_RFU1:
+      Serial.println("EV_RFU1");
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println("EV_JOIN_FAILED");
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println("EV_REJOIN_FAILED");
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println("EV_TXCOMPLETE (Includes waiting for RX windows)");
+
+      if(LMIC.txrxFlags & TXRX_ACK) {
+        Serial.println("Received ACK");
+      }
+
+      if(LMIC.dataLen) {
+        Serial.print("Received ");
+        Serial.print(LMIC.dataLen);
+        Serial.println(" bytes of payload");
+      }
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println("EV_LOST_TSYNC");
+      break;
+    case EV_RESET:
+      Serial.println("EV_RESET");
+      break;
+    case EV_RXCOMPLETE:
+      Serial.println("EV_RXCOMPLETE");
+      break;
+    case EV_LINK_DEAD:
+      Serial.println("EV_LINK_DEAD");
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println("EV_LINK_ALIVE");
+      break;
+    case EV_SCAN_FOUND:
+      Serial.println("EV_SCAN_FOUND");
+      break;
+    case EV_TXSTART:
+      Serial.println("EV_TXSTART");
+      break;
+    case EV_TXCANCELED:
+      Serial.println("EV_TXCANCELED");
+      break;
+    case EV_RXSTART:
+      break;
+    case EV_JOIN_TXCOMPLETE:
+      Serial.println("EV_JOIN_TXCOMPLETE: No JoinAccept");
+      break;
+
+    default:
+      Serial.print("Unknown event: ");
+      Serial.println((unsigned) event);
+      break;
+  }
+}
 
 // Light sensor (BH1750) - https://github.com/claws/BH1750
 #include <BH1750.h>
